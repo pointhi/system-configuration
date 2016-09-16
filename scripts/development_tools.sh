a8e7ca143261cdc0d333c75c4045584d9de21071
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-09-16 11:16:20 +0200

    Add some more development tools

--- scripts/development_tools.sh
+++ scripts/development_tools.sh
@@ -8,6 +8,11 @@ sudo pacman -S jre8-openjdk jdk8-openjdk openjdk8-src openjdk8-doc
 sudo archlinux-java set java-8-openjdk  # use openjdk-8 by default
 
 
+### Python
+
+sudo pacman -S python-pylint python2-pylint
+
+
 ### Some development tools
 
 sudo pacman -S colordiff meld python2-dbus  # diff tools
@@ -15,11 +20,13 @@ sudo pacman -S colordiff meld python2-dbus  # diff tools
 sudo pacman -S git svn # version control
 sudo pacman -S gitg # version control gui
 
+sudo pacman -S ghex # hex viewer
+
 
 ### Editors
 
 sudo pacman -S gedit gedit-plugins gedit-code-assistance clang vala ruby-dbus libgit2-glib gucharmap
-echo "export EDITOR=/usr/bin/gedit" >> ~/.bashrc # set gedit as default editor
+#sudo /bin/su -c "echo 'EDITOR=\"/usr/bin/gedit\"' >> /etc/yaourtrc" # set gedit as default for yaourt # TODO: test change
 
 sudo pacman -S eclipse-java
 

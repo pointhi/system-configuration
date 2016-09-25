8e7e3697863e5335dc74c65e800f21fcfa5fa53c
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-09-25 21:55:38 +0200

    Some extensions of the software park

--- extensions/firefox_extensions.md
+++ extensions/firefox_extensions.md
@@ -1,3 +1,3 @@
-* HTTPS Everywhere
-* NoScript
-* uBlock Origin
+* https://addons.mozilla.org/de/firefox/addon/https-everywhere
+* https://addons.mozilla.org/de/firefox/addon/noscript
+* https://addons.mozilla.org/de/firefox/addon/ublock-origin
--- scripts/some_software.sh
+++ scripts/some_software.sh
@@ -7,6 +7,16 @@ sudo pacman -S thunderbird thunderbird-i18n-de
 
 ### Download Programs
 
-sudo pacman -S transmission-gtk
-yaourt -S jdownloader2
+sudo pacman -S transmission-gtk # torrent downloader
 
+yaourt -S jdownloader2 # general downloader
+
+
+### Media Programs
+
+sudo pacman -S dia # diagram editor
+
+sudo pacman -S krita
+sudo pacman -S gimp
+
+sudo pacman -S inkscape

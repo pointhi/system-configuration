a12b4ff4c9a864e5b4d9fd97381b933c1acdd64b
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-09-25 18:30:15 +0200

    Add some more programs

---
+++ scripts/ham_radio_software.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+### SDR
+
+sudo pacman -S gnuradio gnuradio-companion # General purpose DSP and SDR toolkit
+
+sudo pacman -S gqrx # Interactive SDR receiver waterfall for many devices
+
+
+### Digital Modem Programs
+
+yaourt -S fldigi
+
+
+### Protocols
+
+yaourt -S ax25-tools
+
+
+### Management Programs
+
+yaourt -S chirp # GUI tool for programming ham radios
+
+
+### other Programs
+
+yaourt -S gpredict # Real-time satellite tracking 
---
+++ scripts/some_software.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+### Email Programs
+
+sudo pacman -S thunderbird thunderbird-i18n-de
+
+
+### Download Programs
+
+sudo pacman -S transmission-gtk
+yaourt -S jdownloader2
+

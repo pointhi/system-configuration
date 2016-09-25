477d35dfa572d46a7de649347fb77eca73015eeb
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-09-25 17:48:04 +0200

    Add file template which are used by Nautilus & co.

--- scripts/basic_system.sh
+++ scripts/basic_system.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+PWD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+HOME_DIR="/home/$USER"
+
+SCRIPT_TEMPLATE_DIR="$PWD_DIR/user_templates"
+USER_TEMPLATE_DIR="$HOME_DIR/Vorlagen"  # TODO: more reliable way to get dir?
+
+
 ### Login Manager (GDM)
 
 sudo pacman -S gdm
@@ -10,6 +18,11 @@ sudo systemctl enable gdm.service
 ### Bash
 
 echo "
-# https://askubuntu.com/questions/15926/how-to-avoid-duplicate-entries-in-bash-history
+ https://askubuntu.com/questions/15926/how-to-avoid-duplicate-entries-in-bash-history
 export HISTCONTROL=ignoreboth:erasedups
 " >> ~/.bashrc
+
+
+### Add file templates
+
+cp "$SCRIPT_TEMPLATE_DIR/"* "$USER_TEMPLATE_DIR/"

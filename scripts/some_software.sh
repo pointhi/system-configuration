67843336f49def867e2757676c3e47ac1dd2a437
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-09-30 09:59:49 +0200

    Add graph visualisation software

--- scripts/some_software.sh
+++ scripts/some_software.sh
@@ -14,11 +14,16 @@ yaourt -S jdownloader2 # general downloader
 
 ### Media Programs
 
-sudo pacman -S dia # diagram editor
-
 sudo pacman -S krita
 sudo pacman -S gimp
 
 sudo pacman -S inkscape
 
 sudo pacman -S musescore # Create, play and print beautiful sheet music
+
+
+### Visualisation Programs
+
+sudo pacman -S dia # diagram editor
+
+sudo pacman -S graphviz xdot # graph visualisation
